bdbaadd4e9d5cfca2b8141645c9b49d72ebcb3f5
Michael Polzer <hashrate@pm-tech.at>
2019-02-07 11:03:34 +0100

    use raw strings for regexp input to satisfy linter (#9)
    * Update chaincoin_config.py
    
    * use raw strings for regexp input to satisfy linter

--- lib/chaincoin_config.py
+++ lib/chaincoin_config.py
@@ -15,7 +15,7 @@ class ChaincoinConfig():
         f = io.open("sentinel.conf")
         lines = []
         for line in f:
-            if re.match('^\s*#', line):
+            if re.match(r'^\s*#', line):
                 continue
             lines.append(line)
         f.close()
